e3faf124d5b247dc86680cdc8ad2233f6618d83e
JorgeTC <jorgedelatc@gmail.com>
2024-02-18 19:24:30 +0100

    PATH as property, not classmethod

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from pathlib import Path
 
 
@@ -16,29 +17,36 @@ class SrcImages:
     EXTENSIONS = {'.jpg'}
 
 
+@dataclass(frozen=True)
 class OutFile:
     # Directory to store the output file
-    DIR: Path = None
+    DIR: Path
 
     # Name of the output file
-    NAME: str = None
+    NAME: str
 
-    # Full path to the output file
-    PATH = DIR / NAME
+    # Pixels per inch
+    PPP: int
+
+    # Colour for the space between images
+    BACKGROUND_COLOR: tuple[int, int, int]
+
+    @property
+    def PATH(self):
+        '''Full path to the output file'''
+        return self.DIR / self.NAME
 
 
 RGB_LETTERBOX = (20/255, 24/255, 28/255)
 
 
-class OutImage(OutFile):
-    BACKGROUND_COLOR = RGB_LETTERBOX
-    DIR = Path("./out").resolve()
-    NAME = 'grid.jpg'
-    PPP = 300
+OutImage = OutFile(BACKGROUND_COLOR=RGB_LETTERBOX,
+                   DIR=Path("./out").resolve(),
+                   NAME='grid.jpg',
+                   PPP=300)
 
 
-class DebugImage(OutFile):
-    BACKGROUND_COLOR = RGB_LETTERBOX
-    DIR = Path("./out").resolve()
-    NAME = 'grid_debug.jpg'
-    PPP = 300
+DebugImage = OutFile(BACKGROUND_COLOR=RGB_LETTERBOX,
+                     DIR=Path("./out").resolve(),
+                     NAME='grid_debug.jpg',
+                     PPP=300)
